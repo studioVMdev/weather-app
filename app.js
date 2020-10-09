f9f7c5c8d695a87d650c81fd7c24d6a7b38314eb
Valentin Manaila <valentin.manaila@gmail.com>
2020-10-09 15:13:46 +0100

    Working functionality

--- app.js
+++ app.js
@@ -1,38 +1,55 @@
 const key = "f39943c1400d9f757dc4b90507d5c514";
 const baseURL = 'http://api.weatherstack.com/current?'
 
-// http://api.weatherstack.com/current
-// ? access_key = YOUR_ACCESS_KEY
-// & query = New York
-
-const formEl = document.querySelector('form');
 const details = document.querySelector('.details');
+const getWeatherBtn = document.querySelector('.get-weather');
 
-formEl.addEventListener('submit', (e) => {
+let coords = "";
+
+function getLocation() {
+   if ('geolocation' in navigator) {
+      /* geolocation is available */
+      navigator.geolocation.getCurrentPosition((position) => {
+         coords = position.coords.latitude + "," + position.coords.longitude;
+         weatherApp(coords);
+      });
+   } else {
+      /* geolocation IS NOT available */
+      details.innerHTML = "Geolocation not available";
+   }
+}
+
+//Weather information is delivered on click
+
+// getWeatherBtn.addEventListener('click', (e) => {
+//    e.preventDefault();
+//    getLocation();
+
+// })
+
+//Weather information is delivered on window load
+
+window.addEventListener('load', (e) => {
    e.preventDefault();
-   details.innerHTML = '<h1>Loading</h1>';
-
-   const location = e.target.location.value;
-   weatherApp(location);
-
+   getLocation();
+   
 })
 
-async function weatherApp(location) {
-   const data = await fetchAPI(location);
+
+async function weatherApp() {
+   const data = await fetchAPI(coords);
    generateHTML(data);
 }
 
 
-async function fetchAPI(location) {
-   const queryURL = `${baseURL}access_key=${key}&query=${location}`;
+async function fetchAPI() {
+   const queryURL = `${baseURL}access_key=${key}&query=${coords}`;
    const res = await fetch(queryURL);
    const data = await res.json();
+
    console.log(data);
-   console.log(data.current.weather_icons[0]);
    return data;
 }
-
-console.log(key);
 
 function generateHTML(data) {
    const html = /*html*/ `
@@ -41,11 +58,12 @@ function generateHTML(data) {
    <h2 class="temp">${data.current.temperature}°c</h2>
    <h3 class="status">${data.current.weather_descriptions[0]}</h3>
    <div class="more-info">
-      <p class="info-1">Feels like ${data.current.feelslike}°c</p>
-      <p class="info-2">Precipitation: ${data.current.precip}</p>
-      <p class="info-3">Windpeed ${data.current.wind_speed}</p>
-      <p class="info-4">Visibility ${data.current.visibility}</p>
-      <p class="info-5">Query: ${data.request.query}</p>
+      <p class="info">Feels like ${data.current.feelslike}°c</p>
+      <p class="info">Precipitation: ${data.current.precip}</p>
+      <p class="info">Windpeed ${data.current.wind_speed}</p>
+      <p class="info">Visibility ${data.current.visibility}</p>
+      <p class="info">Your coord are: ${coords}</p>
+      <p class="info">Query: ${data.request.query}</p>
    </div>
 `;
    details.innerHTML = html;
